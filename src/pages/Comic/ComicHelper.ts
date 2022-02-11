5d03388151ed8a2ee6ee237e1cce4f6fa142244f
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-02-11 20:02:00 -0300

    :children_crossing: Set Comic title as comic page title

--- src/pages/Comic/ComicHelper.ts
+++ src/pages/Comic/ComicHelper.ts
@@ -1,15 +1,26 @@
 import { StackScreenProps } from '@react-navigation/stack';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavigationParams } from '../../routes';
 import { getAuthCredentials } from '../../services/api/api';
 import { Comic } from '../../services/api/apiTypes';
 
 export type ComicPageProps = StackScreenProps<NavigationParams, 'Comic'>;
 
-const ComicHelper = ({ route }: ComicPageProps) => {
+const ComicHelper = ({ route, navigation }: ComicPageProps) => {
   const routeParamsComic = route.params.comic;
   const [comic, setComic] = useState<Comic>(routeParamsComic);
   const credentials = getAuthCredentials();
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: comic.title,
+    });
+
+    return () => {
+      // on unmount
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const comicCoverImageURL = `${comic.thumbnail.path}.${comic.thumbnail.extension}?apikey=${credentials.apikey}&ts=${credentials.ts}&hash=${credentials.hash}`;
 
